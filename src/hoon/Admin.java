20fd34bf22cefc22a863a6408252b001668a3151
marine <hollyzero@naver.com>
2020-05-06 19:31:59 +0900

    update

--- src/hoon/Admin.java
+++ src/hoon/Admin.java
@@ -1,22 +1,17 @@
 package hoon;
 
-public class Admin extends MemberInfo{
+public class Admin{
 	
 	boolean loginCheck;		//로그인
 	String adminId;		//관리자 아이디
 	String adminPw;		//관리자 비번
 	
-
-	Admin(String adminId, String adminPw) {
+	Admin(String adminId,String adminPw){
+		
 		this.adminId = adminId;
 		this.adminPw = adminPw;
 		this.loginCheck = false;
 		
 	}
-	@Override
-	void AllInformation() {
-		super.AllInformation();
-	}
-
 
 }
