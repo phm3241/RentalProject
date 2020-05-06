bd06661bd3e507433d5b20b69ab3b7228406dfe3
phm3241 <parkhm3241@gmail.com>
2020-05-06 19:52:58 +0900

    .

--- src/phm/Test3.java
+++ src/phm/Test3.java
@@ -10,48 +10,48 @@ public class Test3 {
 		public static void main(String args[]) {
 //		String today=null;
 		
-		// 시간 더하기
-		Calendar cal = Calendar.getInstance();
-		
-		
-		// 날짜포멧
-		DateFormat format= new SimpleDateFormat ("yyyy/ MM/ dd");
-		Date rentalDate=null;
-		
-		try {
-			rentalDate=format.parse("2020/ 05/ 05");
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		
-		cal.setTime(rentalDate);
-		System.out.println(format.format(cal.getTime()));
-		
-		// 대여일 : 현재시간
-		rentalDate=format.format(cal.getTime());
-		
-		System.out.println(rentalDate);
-		
-		// 반납일 : 대여일+7일
-		cal.add(Calendar.DATE,7);
-		String returnDate=format.format(cal.getTime());
-
+//		// 시간 더하기
+//		Calendar cal = Calendar.getInstance();
+//		
+//		
+//		// 날짜포멧
+//		DateFormat format= new SimpleDateFormat ("yyyy/ MM/ dd");
+//		Date rentalDate=null;
+//		
+//		try {
+//			rentalDate=format.parse("2020/ 05/ 05");
+//		} catch (ParseException e) {
+//			e.printStackTrace();
+//		}
+//		
+//		cal.setTime(rentalDate);
+//		System.out.println(format.format(cal.getTime()));
+//		
+//		// 대여일 : 현재시간
+//		rentalDate=format.format(cal.getTime());
+//		
+//		System.out.println(rentalDate);
+//		
+//		// 반납일 : 대여일+7일
 //		cal.add(Calendar.DATE,7);
 //		String returnDate=format.format(cal.getTime());
-		
-		System.out.println(returnDate);
-
-		
-		// 연장기간 : 대여일 +14일
-		cal.add(Calendar.DATE,7);
-		String dateOfExtens=format.format(cal.getTime());;	// 연장기간#
-
-		System.out.println(dateOfExtens);
-
-		String overdue;			// 연체일#
-		
-		
-		
+//
+////		cal.add(Calendar.DATE,7);
+////		String returnDate=format.format(cal.getTime());
+//		
+//		System.out.println(returnDate);
+//
+//		
+//		// 연장기간 : 대여일 +14일
+//		cal.add(Calendar.DATE,7);
+//		String dateOfExtens=format.format(cal.getTime());;	// 연장기간#
+//
+//		System.out.println(dateOfExtens);
+//
+//		String overdue;			// 연체일#
+//		
+//		
+//		
 
 	
 }
