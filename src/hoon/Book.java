c961e38bcd00d2b77b5644ae3f0c55c90f822b04
marine <hollyzero@naver.com>
2020-05-06 16:30:27 +0900

    RentalItemInfo Start

--- src/hoon/Book.java
+++ src/hoon/Book.java
@@ -3,18 +3,18 @@ package hoon;
 public class Book extends RentalItemInfo{
 	
 	String author;		//저자
-	String limitAge;	//제한연령
+	int limitAge;		//제한연령
 	String story;		//설명
-	String launchDate;	//출판일
+	int launchDate;		//출판일
 	String bestTitle;	//인기자료
-	String rentalCount;	//대여횟수
-	String like;		//관심수
-	String numOfItem;	//재고
-	String returnDate;	//반납예정일
+	int rentalCount;	//대여횟수
+	int like;			//관심수
+	int numOfItem;		//재고
+	int returnDate;		//반납예정일
 	
 
 	//인스턴스 변수 초기
-	Book(String title, String genre, String localData, String rentInfo,String author,String limitAge,String story,String launchDate,String bestTitle) {
+	Book(String title, String genre, String localData, String rentInfo,String author,int limitAge,String story,int launchDate,String bestTitle) {
 		super(title, genre, localData, rentInfo);
 		this.author = author;
 		this.limitAge = limitAge;
@@ -24,7 +24,7 @@ public class Book extends RentalItemInfo{
 		
 	}
 	
-	Book(String title, String genre, String localData, String rentInfo,String author,String limitAge,String story,String launchDate,String bestTitle,String rentalCount,String like,String numOfItem,String returnDate) {
+	Book(String title, String genre, String localData, String rentInfo,String author,int limitAge,String story,int launchDate,String bestTitle,int rentalCount,int like,int numOfItem,int returnDate) {
 		super(title, genre, localData, rentInfo);
 		this.author = author;
 		this.limitAge = limitAge;
@@ -39,16 +39,20 @@ public class Book extends RentalItemInfo{
 	}
 	
 	
-
 	@Override
-	void showInfo() {
-		super.showInfo();
-		System.out.println("저자 :"+author+"\t제한연령 :"+limitAge+"\t설명 :"+story+"\t출판일 :"+launchDate+"\t인기자료 :"+bestTitle);
-		System.out.println("대여횟수 :"+rentalCount+"\t관심수 :"+like+"\t재고 :"+numOfItem+"\t반납예정일 :"+returnDate);
+	void showBasicInfo() {
+		super.showBasicInfo();
+		System.out.println("저자 :"+author);
 		
 	}
 	
 	
-	
+	@Override
+	void showAllinfo() {
+		super.showBasicInfo();
+		System.out.println("제한연령 :"+limitAge+"\t설명 :"+story+"\t출판일 :"+launchDate+"\t인기자료 :"+bestTitle);
+		System.out.println("대여횟수 :"+rentalCount+"\t관심수 :"+like+"\t재고 :"+numOfItem+"\t반납예정일 :"+returnDate);
+		
+	}
 
 }
--- src/hoon/DVD.java
+++ src/hoon/DVD.java
@@ -3,18 +3,18 @@ package hoon;
 public class DVD extends RentalItemInfo{
 	
 	String foreman;		//감독
-	String runingTime;	//상영시간
-	String limitAge;	//제한연령
+	int runingTime;		//상영시간
+	int limitAge;		//제한연령
 	String story;		//설명
-	String launchDate;	//출판일
+	int launchDate;		//출판일
 	String bestTitle;	//인기자료
-	String rentalCount;	//대여횟수
-	String like;		//관심수
-	String numOfItem;	//재고
-	String returnDate;	//반납예정일
+	int rentalCount;	//대여횟수
+	int like;			//관심수
+	int numOfItem;		//재고
+	int returnDate;		//반납예정일
 
 	//인스턴스 변수 초기화
-	DVD(String title, String genre, String localData, String rentInfo,String foreman,String runingTime,String limitAge,String story,String launchDate,String bestTitle) {
+	DVD(String title, String genre, String localData, String rentInfo,String foreman,String runingTime,int limitAge,String story,int launchDate,String bestTitle) {
 		super(title, genre, localData, rentInfo);
 		this.foreman = foreman;
 		this.rentInfo = rentInfo;
@@ -24,7 +24,7 @@ public class DVD extends RentalItemInfo{
 		this.bestTitle = bestTitle;
 	}
 	
-	DVD(String title, String genre, String localData, String rentInfo,String foreman,String runingTime,String limitAge,String story,String launchDate,String bestTitle,String rentalCount,String like,String numOfItem,String returnDate) {
+	DVD(String title, String genre, String localData, String rentInfo,String foreman,String runingTime,int limitAge,String story,int launchDate,String bestTitle,int rentalCount,int like,int numOfItem,int returnDate) {
 		super(title, genre, localData, rentInfo);
 		this.foreman = foreman;
 		this.rentInfo = rentInfo;
@@ -40,12 +40,17 @@ public class DVD extends RentalItemInfo{
 	
 
 	@Override
-	void showInfo() {
-		super.showInfo();
-		System.out.println("감독 :"+foreman+"\t제한연령 :"+limitAge+"\t설명 :"+story+"\t출판일 :"+launchDate+"\t인기자료 :"+bestTitle+"\t상영시간 :"+runingTime);
-		System.out.println("대여횟수 :"+rentalCount+"\t관심수 :"+like+"\t재고 :"+numOfItem+"\t반납예정일 :"+returnDate);
+	void showBasicInfo() {
+		super.showBasicInfo();
+		System.out.println("감독 :"+foreman);
 	}
 	
+	@Override
+	void showAllinfo() {
+		super.showBasicInfo();
+		System.out.println("\t제한연령 :"+limitAge+"\t설명 :"+story+"\t출판일 :"+launchDate+"\t인기자료 :"+bestTitle+"\t상영시간 :"+runingTime);
+		System.out.println("대여횟수 :"+rentalCount+"\t관심수 :"+like+"\t재고 :"+numOfItem+"\t반납예정일 :"+returnDate);
+	}
 	
 
 }
