cb6899dfc645b5eae787c0fbee6e092b330169ad
marine <hollyzero@naver.com>
2020-05-07 11:31:16 +0900

    Book 최신

--- src/hoon/Book.java
+++ src/hoon/Book.java
@@ -5,38 +5,29 @@ public class Book extends RentalItemInfo{
 	String author;		//저자
 	int limitAge;		//제한연령
 	String story;		//설명
-	String launchDate;	//출판일
-	String bestTitle;	//인기자료
-	int rentalCount;	//대여횟수
-	int like;			//관심수
-	int numOfItem;		//재고
-	String returnDate;	//반납예정일
+	int launchDate;		//출판일
+	String bestTitle;	//인기자료	null
+	int rentalCount;	//대여횟수	0
+	int like;			//관심수 		0
+	int numOfItem;		//재고 		5
+	String returnDate;	//반납예정일 	0
 	
 
-	//인스턴스 변수 초기
-	Book(String title, String genre, String localData, String rentInfo,String author,int limitAge,String story,String launchDate,String bestTitle) {
+	//인스턴스 변수 초기화
+	Book(String title, String genre, String localData, String rentInfo,String author,int limitAge,String story,int launchDate) {
 		super(title, genre, localData);
 		this.author = author;
 		this.limitAge = limitAge;
 		this.story = story;
 		this.launchDate = launchDate;
-		this.bestTitle = bestTitle;
+		this.bestTitle = null;
+		this.rentalCount = 0;
+		this.like = 0;
+		this.numOfItem = 5;
+		this.returnDate = null;
 		
 	}
 	
-	Book(String title, String genre, String localData, String rentInfo,String author,int limitAge,String story,String launchDate,String bestTitle,int rentalCount,int like,int numOfItem,String returnDate) {
-		super(title, genre, localData);
-		this.author = author;
-		this.limitAge = limitAge;
-		this.story = story;
-		this.launchDate = launchDate;
-		this.bestTitle = bestTitle;
-		this.rentalCount = rentalCount;
-		this.like = like;
-		this.numOfItem = numOfItem;
-		this.returnDate = returnDate;
-		
-	}
 	
 	
 	@Override
@@ -50,11 +41,10 @@ public class Book extends RentalItemInfo{
 	@Override
 	void showAllinfo() {
 		super.showBasicInfo();
-		System.out.println("제한연령 :"+limitAge+"\t출판일 :"+launchDate+"\t인기자료 :"+bestTitle+"\r\n설명 :"+story);
+		System.out.println("제한연령 :"+limitAge+"\t설명 :"+story+"\t출판일 :"+launchDate+"\t인기자료 :"+bestTitle);
 		System.out.println("대여횟수 :"+rentalCount+"\t관심수 :"+like+"\t재고 :"+numOfItem+"\t반납예정일 :"+returnDate);
 		
 	}
 	
 	
-
 }
