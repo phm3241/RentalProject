d995e61489bcd551cbf24519e59cbfd72e0b0f3c
marine <hollyzero@naver.com>
2020-05-07 19:45:16 +0900

    개인회원 및 자료정리

--- src/data/DVDData2.java
+++ src/data/DVDData2.java
@@ -5,16 +5,17 @@ import hoon.DVD;
 public class DVDData2 extends DVD{
 	
 	public DVDData2(){
-		this.title = "레이니 데이 인 뉴욕";
-		this.genre = "멜로/로맨스";
-		this.localData ="B-1";
-		this.foreman = "우디 앨런";
-		this.runingTime = "92분";
+		this.title = "트롤:월드 투어";
+		this.genre = "애니메이션";
+		this.localData ="B-2";
+		this.foreman = "월트토른데이빗P.스미스";
+		this.runingTime = "91분";
 		this.limitAge = 0;
-		this.story = "상상해 봐요 막 떨어지기 시작한 빗방울 센트럴 파크 델라코트 시계 아래 누군가 당신을 기다리고 있다면… 재즈를 사랑하는 ‘개츠비’(티모시 샬라메)\r\n" + 
-				"영화에 푹 빠진 ‘애슐리’(엘르 패닝) 낭만을 꿈꾸는 ‘챈’(셀레나 고메즈) 매력적인 세 남녀가 선사하는 낭만적인 하루!\r\n" + 
-				" 운명 같은 만남을 기대하며 봄비 내리는 뉴욕에서 로맨틱한 하루를 함께 하실래요?";
-		this.launchDate = "2020.05.06";
+		this.story = "팝, 록, 클래식, 컨트리, 펑크, 테크노, K-POP까지\r\n" + 
+				"세상의 모든 음악이 쏟아진다! 노래와 춤을 즐기며 평화로운 나날을 보내던 팝 트롤 ‘파피’와 ‘브랜치’ 그리고 친구들. 어느 날 ‘파피’는 자신들 외에도 서로 다른 외모와 노래를 가진 5개의 트롤 마을이 더 있다는 것을 알게 된다.\r\n" + 
+				"모두와 친구가 되어 신나게 지내고 싶은 ‘파피’와 달리, 록 트롤 마을의 여왕 ‘바브’는 록을 제외한 모든 음악을 없애기 위해 다른 트롤 마을들을 하나씩 파괴하기 시작하고\r\n" + 
+				"‘파피’는 위기에 빠진 트롤 세계를 구하기 위해 친구들과 위험천만한 모험을 떠나게 되는데…";
+		this.launchDate = "2020.04.29";
 		//this.bestTitle = "";
 		this.rentalCount = 0;
 		this.like = 0;
