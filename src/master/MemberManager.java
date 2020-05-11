aad7455dbaf9adf315037adab5e35b502106448a
phm3241 <parkhm3241@gmail.com>
2020-05-11 16:04:13 +0900

    박혜미_관리자 수정중

--- src/master/MemberManager.java
+++ src/master/MemberManager.java
@@ -152,7 +152,6 @@ public class MemberManager {
 					/* 예약 메서드 */
 					break;
 				case 3: 
-					searchItemInfo();
 					break;
 				default:
 					System.out.println("1~3사이의 숫자를 입력해주세요.");
@@ -732,12 +731,14 @@ public class MemberManager {
 // ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 	// 이용안내
 	void showGuide() {
-		System.out.println("===============================\t\t이\t\t용\t\t안\t\t내============================================");
+		System.out.println("========================\t\t이\t용\t안\t내\t\t========================");
 		System.out.println();
 		System.out.println("1. 비회원은 대여 검색을 하여 대여 가능한 목록을 볼수 있으며, 대여 하기위해서는 회원가입을 해야지만 대여 가능합니다.");
 		System.out.println("2. 회원가입은 이름, 나이, 전화번호, 주소, 이메일 그리고 아이디와 패스워드 입력을 통해서 회원가입이 합니다.");
 		System.out.println("3. 회원가입이 완료 되었으면 로그인을 하고 도서, DVD, 게임 중 목록을 보고 선택하여 대여를 할수 있습니다.");
 		System.out.println("4. 개인회원은 도서, DVD, 게임 품목은 기본정보와 대여가능횟수, 관심수, 재고, 반납예정일을 화면상에서 확인 할수 있습니다.");
+		System.out.println();
+		System.out.println();
 		System.out.println("대여자료 : 도서 / DVD / 게임");
 		System.out.println("대여가능 자료수 : id당 5개");
 		System.out.println("대여기간 : 자료당 7일");
