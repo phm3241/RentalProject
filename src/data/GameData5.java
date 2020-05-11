a0a5e477bda6643adb7e40ab5cc5fd5fcf6aad04
marine <hollyzero@naver.com>
2020-05-11 14:42:44 +0900

    게임자료 수정

--- src/data/GameData5.java
+++ src/data/GameData5.java
@@ -5,13 +5,12 @@ import master.Game;
 public class GameData5 extends Game{
 	
 	public GameData5(){
-		this.title = "포켓몬스터";
-		this.genre = "롤플레잉 비디오게임";
+		this.title = "별의커비";
+		this.genre = "액션게임";
 		this.localData = "G-5";
 		this.producer = "닌텐도";
 		this.limitAge = 0;
-		this.story = "포켓몬스터 레츠고! 피카츄와 포켓몬스터 레츠고! 이브이는 2018년 11월 16일에 발매된 포켓몬스터 시리즈의 닌텐도 스위치용 롤플레잉 비디오 게임으로 게임프리크에서 개발, \r\n" + 
-				"닌텐도와 포켓몬 주식회사에서 배급한다.";
+		this.story = "별의 커비는 닌텐도가 제작한 비디오 게임 시리즈이다. 원래는 주인공의 이름을 포포포 혹은 《팅클 포포》라는 작품명으로 개발하고 있었지만 후에 “커비”라는 이름으로 변경해 닌텐도가 발매시에는 《별의 커비》란 타이틀로 다시 만들었다.";
 		this.launchDate = "2018.11.16";
 		this.rentalCount = 0;
 		this.like = 0;
