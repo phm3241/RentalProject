df6ce43444769584ed7c3ce6ef5215bf1fd81026
phm3241 <parkhm3241@gmail.com>
2020-05-11 11:26:10 +0900

    박혜미 extenDate 변수명 수정

--- src/master/Member.java
+++ src/master/Member.java
@@ -13,7 +13,7 @@ public class Member extends MemberInfo{
 	public int rentalDate;			//대여일 7
 	public String returnDate;		//반납예정일 
 	public String rentInfo;			//대여상태 
-	public int ExtenDate;		//연장된 반납일 = 반납예정일 +7
+	public int extenDate;		//연장된 반납일 = 반납예정일 +7
 	
 	
 	
@@ -29,7 +29,7 @@ public class Member extends MemberInfo{
 		this.numOfExtens = 1;
 		this.overdue = 0;
 		this.rentalDate = 7;
-		this.ExtenDate = 7;
+		this.extenDate = 7;
 	
 	}
 	public Member() {
@@ -48,7 +48,7 @@ public class Member extends MemberInfo{
 	void AllInformation() {
 		super.showBasicInfo();
 		System.out.println("등급 :"+level+"\t\t\t대여권수 :"+numOfRent+"\t\t\t\t대여가능권수 :"+rentalAvail+"\t\t\t연장가능횟수 :"+numOfExtens+"\t\t\t연체 :"+overdue);
-		System.out.println("대여일 :"+rentalDate+"\t반납예정일 :"+returnDate+"\t\t\t대여상태 :"+rentInfo+"\t\t\t연장된 반납일 :"+ExtenDate);
+		System.out.println("대여일 :"+rentalDate+"\t반납예정일 :"+returnDate+"\t\t\t대여상태 :"+rentInfo+"\t\t\t연장된 반납일 :"+extenDate);
 	}
 	
 	boolean checkId(String id) {
